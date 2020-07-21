0413ba2a657ac4c5ae56c22f44759a837fb31a4b
hakawa <hakawa.alves@gmail.com>
2020-07-20 21:33:43 -0300

    adiciona listagem de tarefas, novos testes, e ações para concluir/ criar nova tarefa

--- src/components/listarTarefas/index.js
+++ src/components/listarTarefas/index.js
@@ -1,13 +1,40 @@
 import React from "react";
 import { A } from "hookrouter";
+import { Table } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
+import ItensListaTarefas from "./itens-lista-tarefas";
 import "./style.css";
 
-const Listagem = () => {
+const Listagem = ({ tarefas, recarregarTarefas }) => {
   return (
-    <A href="/cadastrar" className="btn btn-success btn-sm">
-      Nova Tarefa
-    </A>
+    <div className="text-center">
+      <h3> Tarefas a fazer </h3>
+      <Table striped bordered hover responsive data-testid="tabela">
+        <thead>
+          <tr>
+            <th>Tarefa</th>
+            <th>
+              <A
+                href="/cadastrar"
+                className="btn btn-success btn-sm"
+                data-testid="btn-nova-tarefa"
+              >
+                <FontAwesomeIcon icon={faPlus} />
+                &nbsp; Nova tarefa
+              </A>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <ItensListaTarefas
+            tarefas={tarefas}
+            recarregarTarefas={recarregarTarefas}
+          />
+        </tbody>
+      </Table>
+    </div>
   );
 };
 
--- src/components/modals/index.js
+++ src/components/modals/index.js
@@ -1,17 +1,46 @@
 import React from "react";
 import { Modal, Button } from "react-bootstrap";
 
-const Resultado = ({ show, onHide, data, title, buttonText }) => {
+const Resultado = ({
+  show,
+  onHide,
+  data,
+  title,
+  buttonText,
+  twoButtons,
+  buttonTextTwo,
+  textTwo,
+  func,
+}) => {
   return (
     <Modal show={show} onHide={onHide} data-testid="modal">
       <Modal.Header closeButton>
         <Modal.Title>{title}</Modal.Title>
       </Modal.Header>
-      <Modal.Body>{data}</Modal.Body>
+      <Modal.Body>
+        {data}
+        {textTwo && (
+          <>
+            <br />
+            <strong>{textTwo}</strong>
+          </>
+        )}
+      </Modal.Body>
       <Modal.Footer>
-        <Button variant="success" onClick={onHide}>
-          {buttonText}
-        </Button>
+        {twoButtons ? (
+          <>
+            <Button variant="primary" data-testid="btn-concluir" onClick={func}>
+              {buttonText}
+            </Button>
+            <Button variant="light" onClick={onHide}>
+              {buttonTextTwo}
+            </Button>
+          </>
+        ) : (
+          <Button variant="success" onClick={onHide}>
+            {buttonText}
+          </Button>
+        )}
       </Modal.Footer>
     </Modal>
   );
